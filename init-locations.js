02a750c72ac53f69d3c37e0119e5bd31801da3ca
Yashwanth <yashwanthdamera6@gmail.com>
2021-01-27 17:52:15 -0600

    Geolocations

---
+++ init-locations.js
@@ -0,0 +1,14 @@
+
+export default [
+    {
+        Name: 'Randolph Road',
+        Latitude: 39.1616601,
+        Longitude: -94.4902875
+    },
+    {
+        Name: 'Wyandotte Street',
+        Latitude: 39.190385,
+        Longitude:-94.493967
+    }
+    
+];
