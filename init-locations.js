c31b92035b2e8f2da9e1289960027fb7d022c484
Yashwanth <yashwanthdamera6@gmail.com>
2021-02-07 00:56:38 -0600

    added location

--- init-locations.js
+++ init-locations.js
@@ -8,7 +8,16 @@ export default [
     {
         Name: 'Horizons',
         Latitude: 40.356062,
-        Longitude:-94.881527
+        Longitude: -94.881527
+    },
+
+
+    {
+        Name: 'McCrimon Parkway',
+        Latitude: 35.8416,
+        Longitude: -78.852915,
+        South: 35.8425,
+        East: -78.844,
     }
-    
+
 ];
